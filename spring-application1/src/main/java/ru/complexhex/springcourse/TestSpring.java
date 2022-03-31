12058a65c804d02b33041ba38823a7e143845e84
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-03-31 06:45:30 +0300

    v10

--- spring-application1/src/main/java/ru/complexhex/springcourse/TestSpring.java
+++ spring-application1/src/main/java/ru/complexhex/springcourse/TestSpring.java
@@ -8,18 +8,11 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
+//       MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//       musicPlayer.playMusic();
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-        musicPlayer.playMusic();
-
-        Music music1 = context.getBean("classicalMusic",Music.class);
-
-        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
-        musicPlayer1.playMusic();
-
-
-
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
