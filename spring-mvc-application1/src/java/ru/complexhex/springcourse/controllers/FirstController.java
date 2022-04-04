c6dee74d2c9ffdc26cc89184f3eeeb5d335701ca
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-04-04 20:29:18 +0300

    v20

--- spring-mvc-application1/src/java/ru/complexhex/springcourse/controllers/FirstController.java
+++ spring-mvc-application1/src/java/ru/complexhex/springcourse/controllers/FirstController.java
@@ -1,8 +1,11 @@
 package ru.complexhex.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sun.awt.SunHints;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,17 +14,41 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request){
-        String name =  request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
         System.out.println(name + " " + surname);
+//        model.addAttribute("message", name + " " + surname);
 
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage(){
+    public String goodByePage() {
         return "first/goodbye";
+    }
+
+
+    @GetMapping("/calculator")
+    public String doCalculate(@RequestParam(value = "a") int a,
+                              @RequestParam(value = "b") int b,
+                              @RequestParam(value = "action") String action,
+                              Model model) {
+        int i = 0;
+        switch (action) {
+            case "multi":
+                i = a * b;
+                break;
+
+            case "add":
+                i = a + b;
+                break;
+        }
+
+
+        model.addAttribute("calc", i);
+
+        return "first/calculator";
     }
 }
