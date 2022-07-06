9ea43777ed30e65b88e0a3d0d4bf7ad54db6bd11
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-07-06 22:01:24 +0300

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,66 @@
+package org.example;
+//
+//import org.example.model.Person;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//
+///**
+// * Hello world!
+// */
+//public class App {
+//    public static void main(String[] args) {
+//
+//        //по умолчанию читается файл hibernate.properties
+//        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+//
+//        SessionFactory sessionFactory = configuration.buildSessionFactory();
+//        Session session = sessionFactory.getCurrentSession();
+//
+//        try {
+//            session.beginTransaction();
+//            Person person = session.get(Person.class, 1);
+//            System.out.println(person.getName());
+//            System.out.println(person.getAge());
+//
+//
+//            session.getTransaction().commit();
+//
+//
+//        } finally {
+//            sessionFactory.close();
+//        }
+//    }
+//}
+
+
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+           Person person = session.get(Person.class, 2);
+           session.delete(person);
+
+
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
