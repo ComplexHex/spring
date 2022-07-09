a9c07c6b095c1c4d5f949dc33493d7bc60d4be06
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-07-09 08:44:20 +0300

    Add files via upload

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,44 +1,14 @@
 package org.example;
-//
-//import org.example.model.Person;
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.hibernate.cfg.Configuration;
-//
-///**
-// * Hello world!
-// */
-//public class App {
-//    public static void main(String[] args) {
-//
-//        //по умолчанию читается файл hibernate.properties
-//        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
-//
-//        SessionFactory sessionFactory = configuration.buildSessionFactory();
-//        Session session = sessionFactory.getCurrentSession();
-//
-//        try {
-//            session.beginTransaction();
-//            Person person = session.get(Person.class, 1);
-//            System.out.println(person.getName());
-//            System.out.println(person.getAge());
-//
-//
-//            session.getTransaction().commit();
-//
-//
-//        } finally {
-//            sessionFactory.close();
-//        }
-//    }
-//}
 
-
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -46,23 +16,67 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().
+                addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
 
         try {
             session.beginTransaction();
-//обращение к сущности Person
-//            List<Person> list = session.createQuery("From Person where age > 30").getResultList();
-//            List<Person> list1 = session.createQuery("From Person where name like 'T%'").getResultList();
-           session.createQuery("update Person set name='TEES' where age = 54").executeUpdate();
-//            for (Person p : list) {
-//                System.out.println(p);
+
+//            Person person = session.get(Person.class, 3);
+//            System.out.println(person);
+//            List<Item> items = person.getItems();
+//            System.out.println(items);
+//
+//            Item item = session.get(Item.class, 2);
+//            System.out.println(item);
+//            Person person = item.getOwner();
+//            System.out.println(person);
+
+//            Person person = session.get(Person.class, 2);
+//            Item newItem = new Item("Item from Hibernate", person);
+////для создания двустороннего отношения между таблицами в кзше hibernate
+//            person.getItems().add(newItem);
+//            session.save(newItem);
+
+            //добавление человека и товара
+//            Person person = new Person("Basil", 33);
+//            Item item = new Item("Uzi", person);
+//            person.setItems(new ArrayList<Item>(Collections.singletonList(item)));
+//            session.save(person);
+//            session.save(item);
+
+            //удаление у человека всех товаров
+//            Person person = session.get(Person.class, 2);
+//            List<Item> items = person.getItems();
+            //SQL
+//            for (Item i:items){
+//                session.remove(i);
 //            }
-//            for (Person p : list1) {
-//                System.out.println(p);
-//            }
+            //для создания двустороннего отношения между таблицами в кзше hibernate
+//            person.getItems().clear();
+
+            //удаление человека
+//            Person person = session.get(Person.class, 1);
+//            session.delete(person);
+            //SQL
+//            session.remove(person);
+            //для создания двустороннего отношения между таблицами в кзше hibernate
+//            person.getItems().forEach(i->i.setOwner(null));
+
+            //замена товара
+            Person person = session.get(Person.class, 3);
+            Item item = session.get(Item.class, 1);
+
+            item.getOwner().getItems().remove(item);
+
+            //SQL
+            item.setOwner(person);
+
+            person.getItems().add(item);
+
 
             session.getTransaction().commit();
 
