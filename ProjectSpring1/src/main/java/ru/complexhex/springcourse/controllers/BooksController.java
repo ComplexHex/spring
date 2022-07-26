b41ad19ff60e962d029eff6e60a674e5ebc891cc
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-07-26 06:39:27 +0300

    Add files via upload

--- ProjectSpring1/src/main/java/ru/complexhex/springcourse/controllers/BooksController.java
+++ ProjectSpring1/src/main/java/ru/complexhex/springcourse/controllers/BooksController.java
@@ -10,7 +10,7 @@ import ru.complexhex.springcourse.dao.BookDAO;
 import ru.complexhex.springcourse.dao.PersonDAO;
 import ru.complexhex.springcourse.models.Book;
 import ru.complexhex.springcourse.models.Person;
-import ru.complexhex.springcourse.services.BookService;
+import ru.complexhex.springcourse.services.BooksService;
 import ru.complexhex.springcourse.services.PeopleService;
 
 import javax.validation.Valid;
@@ -23,12 +23,12 @@ import java.util.Optional;
 @RequestMapping("/books")
 public class BooksController {
 
-    private final BookService bookService;
+    private final BooksService booksService;
     private final PeopleService peopleService;
 
     @Autowired
-    public BooksController(BookService bookService, PeopleService peopleService) {
-        this.bookService = bookService;
+    public BooksController(BooksService booksService, PeopleService peopleService) {
+        this.booksService = booksService;
         this.peopleService = peopleService;
     }
 
@@ -38,22 +38,25 @@ public class BooksController {
                         @RequestParam(value = "sort_by_year", required = false) boolean sortByYear) {
 
         if (page == null || booksPerPage == null) {
+            model.addAttribute("books", booksService.findAll(sortByYear));
 
-        }
+        }else model.addAttribute("books", booksService.findWithPagination(page, booksPerPage, sortByYear));
 
         return "books/index";
     }
 
+
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model, @ModelAttribute("person") Person person) {
-        model.addAttribute("book", bookDAO.show(id));
+        model.addAttribute("book", booksService.findOne(id));
 
-        Optional<Person> bookOwner = bookDAO.getBookOwner(id);
+        Person bookOwner = booksService.getBookOwner(id);
 
-        if (bookOwner.isPresent())
-            model.addAttribute("owner", bookOwner.get());
+        if (bookOwner != null)
+            model.addAttribute("owner", bookOwner);
         else
-            model.addAttribute("people", personDAO.index());
+            model.addAttribute("people", peopleService.findAll());
 
         return "books/show";
     }
@@ -69,13 +72,13 @@ public class BooksController {
         if (bindingResult.hasErrors())
             return "books/new";
 
-        bookDAO.save(Book);
+        booksService.save(Book);
         return "redirect:/books";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("book", bookDAO.show(id));
+        model.addAttribute("book", booksService.findOne(id));
         return "books/edit";
     }
 
@@ -85,25 +88,37 @@ public class BooksController {
         if (bindingResult.hasErrors())
             return "books/edit";
 
-        bookDAO.update(id, book);
+        booksService.update(id, book);
         return "redirect:/books";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
-        bookDAO.delete(id);
+        booksService.delete(id);
         return "redirect:/books";
     }
 
     @PatchMapping("/{id}/release")
     public String release(@PathVariable("id") int id) {
-        bookDAO.release(id);
+        booksService.release(id);
         return "redirect:/books/" + id;
     }
 
     @PatchMapping("/{id}/assign")
     public String assign(@PathVariable("id") int id, @ModelAttribute("person") Person selectedPerson) {
-        bookDAO.assign(id, selectedPerson);
+        // У selectedPerson назначено только поле id, остальные поля - null
+        booksService.assign(id, selectedPerson);
         return "redirect:/books/" + id;
+    }
+
+    @GetMapping("/search")
+    public String searchPage() {
+        return "books/search";
+    }
+
+    @PostMapping("/search")
+    public String makeSearch(Model model, @RequestParam("query") String query) {
+        model.addAttribute("books", booksService.searchByTitle(query));
+        return "books/search";
     }
 }
